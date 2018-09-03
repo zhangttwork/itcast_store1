739acb914634c6ffdec06501745799dd0b14ae1b
zhangttwork <1165816178@qq.com>
2018-09-03 15:05:06 +0800

    Revert "登录时间"
    This reverts commit 9afdea02991b498f4688c8d37a6fc482bf2b4247.

--- src/main.js
+++ src/main.js
@@ -9,10 +9,7 @@ import ElementUI from 'element-ui';
 // import Vue from 'vue';
 // import ElementUI from 'element-ui';
 import 'element-ui/lib/theme-chalk/index.css';
-import '@/assets/css/index.css';
-import MyHttp from '@/plugins/MyHttp';
-
-Vue.use(MyHttp);
+// import App from './App.vue';
 
 Vue.use(ElementUI);
 
