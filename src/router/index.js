b91ab25b3f936745ecfa9557b3677d785c972f28
zhangttwork <1165816178@qq.com>
2018-09-03 11:47:29 +0800

    配置

--- src/router/index.js
+++ src/router/index.js
@@ -1,15 +1,17 @@
-import Vue from 'vue'
-import Router from 'vue-router'
-import HelloWorld from '@/components/HelloWorld'
+import Vue from 'vue';
+import Router from 'vue-router';
+// import HelloWorld from '@/components/HelloWorld';
 
-Vue.use(Router)
+Vue.use(Router);
+// @为src文件夹
+// build 目录下 webpack.base.conf 第38行配置的
+// 
+// 省略 .js 的情况
+// 加载规则：先找 .js / .vue /.json webpack.base.conf 35行
+import Login from '@/views/Login.vue';
 
 export default new Router({
   routes: [
-    {
-      path: '/',
-      name: 'HelloWorld',
-      component: HelloWorld
-    }
+    {name:'login',path:'/login',component:Login}
   ]
-})
+});
